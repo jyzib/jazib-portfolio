06347e9c8120cb4285691049267cce463566f226
jyzib <jazibzaidi02@gmail.com>
2024-04-26 21:44:30 +0530

    first commit

--- components/Blocks/Mobile.jsx
+++ components/Blocks/Mobile.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { ContainerScroll } from "../ui/container-scroll-animation";
+import { ContainerScroll } from "../UI/container-scroll-animation";
 import Image from "next/image";
 
  function HeroScrollDemo() {
